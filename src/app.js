8c7aa80a033cc047ab7849fd4bfe29b9697f9e11
H-Taliby <habibataliby@gmail.com>
2025-11-26 17:01:56 +0100

    added CORS middleware

--- src/app.js
+++ src/app.js
@@ -12,7 +12,13 @@ app.use(morgan('dev'));
 app.use(cors());
 app.use(express.json({ limit: '10mb' }));
 app.use(express.urlencoded({ extended: true }));
+app.use(cors({
+  origin: ["https://ecbi-frontend-9ww4.vercel.app"], // your frontend domain
+  methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
+  credentials: true,
+}));
 
+app.use(express.json());
 app.use('/api', apiRouter);
 
 app.get('/', (req, res) => res.send('ECBI API'));
