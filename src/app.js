e62eb29a88a329809c321cd5a47b3f1dc67a97ab
H-Taliby <habibataliby@gmail.com>
2025-11-26 17:23:20 +0100

    fixed CORS config

--- src/app.js
+++ src/app.js
@@ -6,19 +6,25 @@ const helmet = require('helmet');
 const apiRouter = require('./routes/index.routes');
 
 const app = express();
-
-app.use(helmet());
-app.use(morgan('dev'));
 app.use(cors({
   origin: ["https://ecbi-frontend-9ww4.vercel.app"], // your frontend domain
   methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
   credentials: true,
 }));
+
+app.options('*', cors({
+  origin: ["https://ecbi-frontend-9ww4.vercel.app"],
+  credentials: true
+}));
+
+app.use(helmet());
+app.use(morgan('dev'));
+
 app.use(express.json({ limit: '10mb' }));
 app.use(express.urlencoded({ extended: true }));
 
 
-app.use(express.json());
+
 app.use('/api', apiRouter);
 
 app.get('/', (req, res) => res.send('ECBI API'));
