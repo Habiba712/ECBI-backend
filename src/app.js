8fc54ef1c04291267134bbf70974de8f2784b3ca
H-Taliby <habibataliby@gmail.com>
2025-11-26 20:20:33 +0100

    fixed CORS config

--- src/app.js
+++ src/app.js
@@ -8,20 +8,24 @@ const apiRouter = require('./routes/index.routes');
 const app = express();
 app.use(express.urlencoded({ extended: true }));
 
-app.use(cors({
 
-origin: true,
 
-credentials: true,
+//     const corsOptions = {
+//         origin: ['http://localhost:3000', 'https://ecbi-frontend-9ww4.vercel.app'], // Replace with your actual frontend origins
+//         methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
+//         credentials: true, // Allow cookies and authentication headers
+//         optionsSuccessStatus: 204 // For preflight requests
+//     };
+// app.use(cors(corsOptions));
+   app.use(cors({
+            origin: 'https://ecbi-frontend-9ww4.vercel.app', // Replace with your actual frontend domain
+            methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
+            credentials: true, // If you're using cookies or authorization headers
+            optionsSuccessStatus: 204 // Recommended for preflight requests
+        }));
 
-}));
-
-    const corsOptions = {
-        origin: ['http://localhost:3000', 'https://ecbi-frontend-9ww4.vercel.app'], // Replace with your actual frontend origins
-        methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
-        credentials: true, // Allow cookies and authentication headers
-        optionsSuccessStatus: 204 // For preflight requests
-    };
+        // Or, to allow all origins (for development, use with caution in production)
+        // app.use(cors());
 app.use(helmet());
 app.use(morgan('dev'));
 
